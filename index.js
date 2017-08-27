86043a341593c101f304675ff1ff8c7481a97923
Tomas Dulka <dulajz@gmail.com>
2017-08-27 12:08:15 +0200

    should work now - added regex matching to get the first preffered language and to match the operating system

--- index.js
+++ index.js
@@ -6,18 +6,15 @@ var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
   var ip = req.ip;
-  var language = req.headers["accept-language"];
-  var software = req.headers["user-agent"].match(/\((.+?)\)/)[1];
-
-  console.log(req);
-  console.log(req.ip);
+  var language = req.headers["accept-language"].match(/(.*?)[;,]/)[1];
+  var software = req.headers["user-agent"].match(/\((.*?)\)/)[1];
 
   var response = {
     "ipaddress": req.ip,
     "language": language,
     "software": software
   }
-
+  res.writeHead(200, {"Content-Type": "text/plain"});
   res.end(JSON.stringify(response));
 });
 
